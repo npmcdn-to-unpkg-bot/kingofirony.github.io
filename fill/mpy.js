8c883d584b7d02a0497263802b2e01f8d9676aec
kingofirony <kingofirony@gmail.com>
2016-07-12 16:00:12 -0400

    saf

--- fill/mpy.js
+++ fill/mpy.js
@@ -101,6 +101,7 @@ var nodeChangedFunction;
 
 
 function renderMapzenMap() {
+  var map = L.map('map')
    var layer = Tangram.leafletLayer({
        scene: 'scene.yaml',
        attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>'
